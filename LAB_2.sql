c40b77503ccbd08f21fe9f2efbbff0ddcf6ddeb0
trufflesmld <trufflesmld@gmail.com>
2021-09-27 23:09:36 +0300

    upd initial tasks

--- LAB_2.sql
+++ LAB_2.sql
@@ -1,14 +1,19 @@
-/*«Текущие этапы работы» (условная выборка);*/
+SET
+    linesize 500
+    /*«Текущие этапы работы» (условная выборка);*/
 SELECT
     B_S.*,
+    b.typeobj,
     s.stagename
 FROM
     B_S,
-    Stages s
+    Stages s,
+    buildings b
 WHERE
-    B_S.stagekey = s.stagekey;
+    B_S.stagekey = s.stagekey
+    AND B_S.buildKey = b.buildKey;
 
-/*«Сроки строительства объектов» / Кол-во затраченного материала на объект (итоговый запрос);*/
+/*«Сроки строительства объектов» / Кол-во затраченного материала на объект (итоговый запрос); +*/
 SELECT
     b.buildKey,
     SUM(neededstuf) AS total_stuff
@@ -20,7 +25,7 @@ WHERE
 GROUP BY
     b.buildKey;
 
-/*«Объекты заданного процента завершенности» (параметрический запрос);*/
+/*«Объекты заданного процента завершенности» (параметрический запрос); +*/
 SELECT
     B_S.*,
     s.stagename
@@ -31,7 +36,7 @@ WHERE
     B_S.stagekey = s.stagekey
     AND B_S.stagekey = & y_stagekey;
 
-/*«Общий список объектов и стройматериалов с указанием количества этапов» (запрос на объединение);*/
+/*«Общий список объектов и стройматериалов с указанием количества этапов» (запрос на объединение); не вывез*/
 SELECT
     stagename
 FROM
@@ -42,7 +47,11 @@ SELECT
 FROM
     stuf;
 
-/*«Количество построенных объектов по кварталам» сделанных во втором квартале (запрос по полю с типом дата)*/
-ALTER SESSION SET NLS_DATE_FORMAT = 'DD.MM.YY HH24:MI:SS';
-SELECT COUNT(buildKey) AS Finished_buildings FROM buildings WHERE TO_CHAR(enddate, 'MM') BETWEEN &init_month  AND &end_month;
-
+/*«Количество построенных объектов по кварталам» (запрос по полю с типом дата)*/
+SELECT
+    TO_CHAR(enddate, 'q') AS quarter,
+    COUNT(buildKey) AS Finished_buildings
+FROM
+    buildings
+GROUP BY
+    TO_CHAR(enddate, 'q');
