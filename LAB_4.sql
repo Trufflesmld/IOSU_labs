da124fcb39c0b6bab904c1b7809f65a38d9d1300
trufflesmld <trufflesmld@gmail.com>
2021-10-10 21:54:25 +0300

    upd loop

--- LAB_4.sql
+++ LAB_4.sql
@@ -2,6 +2,7 @@
 SET
     SERVEROUTPUT ON;
 
+-- * Вспомогательная таблица
 CREATE TABLE buildings_vspomog(
     buildKey NUMBER,
     typeObj VARCHAR2(15) NOT NULL,
@@ -12,8 +13,7 @@ CREATE TABLE buildings_vspomog(
     contractPrice number(38, 2)
 );
 
-/*
- *Создать процедуру, копирующую строки с информацией о строительстве в текущем месяце во вспомогательную таблицу. Подсчитать количество извлеченных строк.*/
+-- *Создать процедуру, копирующую строки с информацией о строительстве в текущем месяце во вспомогательную таблицу. Подсчитать количество извлеченных строк.
 CREATE
 OR REPLACE PROCEDURE buildings_current_month IS Today DATE DEFAULT SYSDATE;
 
@@ -38,12 +38,13 @@ err EXCEPTION;
 
 BEGIN OPEN required_buildings;
 
+FETCH required_buildings INTO line_atr;
+
 IF required_buildings % notfound THEN RAISE err;
-end if;
 
-LOOP FETCH required_buildings INTO line_atr;
+END IF;
 
-EXIT
+LOOP EXIT
 WHEN required_buildings % notfound;
 
 counter_buildings := required_buildings % rowcount;
@@ -61,11 +62,14 @@ VALUES
         line_atr.contractPrice
     );
 
+FETCH required_buildings INTO line_atr;
+
 END LOOP;
+commit;
 
 close required_buildings;
 
-DBMS_OUTPUT.PUT_LINE('Количество: ' || counter_buildings);
+DBMS_OUTPUT.PUT_LINE('Количество объектов:' || counter_buildings);
 
 EXCEPTION
 WHEN STORAGE_ERROR THEN RAISE_APPLICATION_ERROR(-6500, 'Не хватает оперативной памяти!');
@@ -74,6 +78,6 @@ WHEN err THEN DBMS_OUTPUT.PUT_LINE('В этом месяце нет строит
 
 END;
 
-/
-/* 
- *Создать функцию, подсчитывающую, сколько этапов выполнено по каждому объекту. Вернуть количество объектов, по которым завершены все этапы.*/
+/ 
+
+-- *Создать функцию, подсчитывающую, сколько этапов выполнено по каждому объекту. Вернуть количество объектов, по которым завершены все этапы.
