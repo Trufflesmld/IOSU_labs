5921ba6b69655385eda4be717481a83fe83a8b28
trufflesmld <trufflesmld@gmail.com>
2021-10-12 02:17:18 +0300

    upd ovearload procedure || add package + packageBody

--- LAB_4.sql
+++ LAB_4.sql
@@ -281,7 +281,10 @@ FROM
 --!-----------------------------------------------------------------------------------------------------------------------------------------------------
 -- * Перегруз 
 --!-----------------------------------------------------------------------------------------------------------------------------------------------------
-CREATE OR REPLACE PROCEDURE buildings_current_month(month_num in number, year_num in number) IS
+CREATE OR REPLACE PROCEDURE buildings_current_month (
+    month_num IN NUMBER,
+    year_num  IN NUMBER
+) IS
 
     counter_buildings NUMBER;
     line_atr          buildings%rowtype;
@@ -291,7 +294,18 @@ CREATE OR REPLACE PROCEDURE buildings_current_month(month_num in number, year_nu
     FROM
         buildings
     WHERE
-        buildings.enddate > to_date(to_char('01.'||month_num||'.'||year_num), 'dd.mm.yyyy');
+        to_char(
+            buildings.enddate
+        ) > to_date(
+            to_char(month_num
+                    || '.'
+                    || year_num), 'mm.yyyy'
+        )
+        AND buildings.contraktdate < ( to_date(
+            to_char(month_num + 1
+                    || '.'
+                    || year_num), 'mm.yyyy'
+        ) - 1 );
 
     err EXCEPTION;
 BEGIN
@@ -329,3 +343,275 @@ EXCEPTION
         dbms_output.put_line('В этом месяце нет строительств');
 END;
 / 
+
+
+--!-----------------------------------------------------------------------------------------------------------------------------------------------------
+-- * Пакет 
+--!-----------------------------------------------------------------------------------------------------------------------------------------------------
+--Create a new Package
+
+CREATE PACKAGE pack_test IS
+
+  -- Add a procedure
+    PROCEDURE buildings_current_month;
+
+    PROCEDURE buildings_current_month (
+        month_num IN NUMBER,
+        year_num  IN NUMBER
+    );
+
+  -- Add a function
+    FUNCTION all_money_of_build (
+        bk buildings.buildkey%TYPE
+    ) RETURN stuf.price_of_stuf%TYPE;
+
+    FUNCTION all_money_of_stage_in_build (
+        bk     buildings.buildkey%TYPE,
+        stagek stages.stagekey%TYPE
+    ) RETURN stuf.price_of_stuf%TYPE;
+
+END pack_test;
+/
+
+--Create a new Package Body
+
+CREATE or REPLACE PACKAGE BODY pack_test IS
+
+  -- Add procedure body
+    PROCEDURE buildings_current_month IS
+
+        counter_buildings NUMBER;
+        line_atr          buildings%rowtype;
+        CURSOR required_buildings IS
+        SELECT
+            *
+        FROM
+            buildings
+        WHERE
+            buildings.enddate > (
+                SELECT
+                    sysdate
+                FROM
+                    dual
+            );
+
+        err EXCEPTION;
+    BEGIN
+        OPEN required_buildings;
+        FETCH required_buildings INTO line_atr;
+        IF required_buildings%notfound THEN
+            RAISE err;
+        END IF;
+        LOOP
+            EXIT WHEN required_buildings%notfound;
+            counter_buildings := required_buildings%rowcount;
+            INSERT INTO buildings_vspomog VALUES (
+                line_atr.buildkey,
+                line_atr.typeobj,
+                line_atr.clientkey,
+                line_atr.teamkey,
+                line_atr.contraktdate,
+                line_atr.enddate,
+                line_atr.contractprice
+            );
+
+            FETCH required_buildings INTO line_atr;
+        END LOOP;
+
+        COMMIT;
+        CLOSE required_buildings;
+        dbms_output.put_line('Количество объектов:' || counter_buildings);
+    EXCEPTION
+        WHEN storage_error THEN
+            raise_application_error(
+                                   -6500,
+                                   'Не хватает оперативной памяти!'
+            );
+        WHEN err THEN
+            dbms_output.put_line('В этом месяце нет строительств');
+    END buildings_current_month;
+
+    PROCEDURE buildings_current_month (
+        month_num IN NUMBER,
+        year_num  IN NUMBER
+    ) IS
+
+        counter_buildings NUMBER;
+        line_atr          buildings%rowtype;
+        CURSOR required_buildings IS
+        SELECT
+            *
+        FROM
+            buildings
+        WHERE
+            to_char(
+                buildings.enddate
+            ) > to_date(
+                to_char(month_num
+                        || '.'
+                        || year_num), 'mm.yyyy'
+            )
+            AND buildings.contraktdate < ( to_date(
+                to_char(month_num + 1
+                        || '.'
+                        || year_num), 'mm.yyyy'
+            ) - 1 );
+
+        err EXCEPTION;
+    BEGIN
+        OPEN required_buildings;
+        FETCH required_buildings INTO line_atr;
+        IF required_buildings%notfound THEN
+            RAISE err;
+        END IF;
+        LOOP
+            EXIT WHEN required_buildings%notfound;
+            counter_buildings := required_buildings%rowcount;
+            INSERT INTO buildings_vspomog VALUES (
+                line_atr.buildkey,
+                line_atr.typeobj,
+                line_atr.clientkey,
+                line_atr.teamkey,
+                line_atr.contraktdate,
+                line_atr.enddate,
+                line_atr.contractprice
+            );
+
+            FETCH required_buildings INTO line_atr;
+        END LOOP;
+
+        COMMIT;
+        CLOSE required_buildings;
+        dbms_output.put_line('Количество объектов:' || counter_buildings);
+    EXCEPTION
+        WHEN storage_error THEN
+            raise_application_error(
+                                   -6500,
+                                   'Не хватает оперативной памяти!'
+            );
+        WHEN err THEN
+            dbms_output.put_line('В этом месяце нет строительств');
+    END buildings_current_month;
+
+
+
+
+  -- Add function body
+    FUNCTION all_money_of_stage_in_build (
+        bk     buildings.buildkey%TYPE,
+        stagek stages.stagekey%TYPE
+    ) RETURN stuf.price_of_stuf%TYPE IS
+
+        allmoney_of_stage stuf.price_of_stuf%TYPE DEFAULT 0;
+        CURSOR curs_stufkey_needed IS
+        SELECT
+            stufkey,
+            neededstuf
+        FROM
+            s_s
+        WHERE
+            s_s.buildkey = bk
+            AND s_s.stagekey = stagek;
+
+        stufkey_needed    curs_stufkey_needed%rowtype;
+        price_stuf        stuf.price_of_stuf%TYPE;
+        name_of_stage     stages.stagename%TYPE;
+        err EXCEPTION;
+    BEGIN
+        SELECT
+            stagename
+        INTO name_of_stage
+        FROM
+            stages
+        WHERE
+            stages.stagekey = stagek;
+
+        OPEN curs_stufkey_needed;
+        FETCH curs_stufkey_needed INTO stufkey_needed;
+        IF curs_stufkey_needed%notfound THEN
+            RAISE err;
+        END IF;
+        LOOP
+            EXIT WHEN curs_stufkey_needed%notfound;
+            SELECT
+                price_of_stuf
+            INTO price_stuf
+            FROM
+                stuf
+            WHERE
+                stuf.stufkey = stufkey_needed.stufkey;
+
+            allmoney_of_stage := allmoney_of_stage + price_stuf * stufkey_needed.neededstuf;
+            FETCH curs_stufkey_needed INTO stufkey_needed;
+        END LOOP;
+
+        dbms_output.put_line('По этапу '
+                             || name_of_stage
+                             || ' было затрачено '
+                             || allmoney_of_stage
+                             || ' денежных единиц');
+
+        RETURN allmoney_of_stage;
+    EXCEPTION
+        WHEN timeout_on_resource THEN
+            raise_application_error(
+                                   -20002,
+                                   'Превышен интервал ожидания'
+            );
+        WHEN value_error THEN
+            raise_application_error(
+                                   -20004,
+                                   'Ошибка в операции преобразования или математической операции!'
+            );
+        WHEN err THEN
+            dbms_output.put_line('По этому строительству пока что нет заказов на материалы');
+    END all_money_of_stage_in_build;
+
+    FUNCTION all_money_of_build (
+        bk buildings.buildkey%TYPE
+    ) RETURN stuf.price_of_stuf%TYPE IS
+
+        allmoney    stuf.price_of_stuf%TYPE DEFAULT 0;
+        CURSOR curs_build_stage IS
+        SELECT
+            *
+        FROM
+            b_s
+        WHERE
+            b_s.buildkey = bk;
+
+        build_stage curs_build_stage%rowtype;
+        err EXCEPTION;
+    BEGIN
+        OPEN curs_build_stage;
+        FETCH curs_build_stage INTO build_stage;
+        IF curs_build_stage%notfound THEN
+            RAISE err;
+        END IF;
+        LOOP
+            EXIT WHEN curs_build_stage%notfound;
+            allmoney := allmoney + all_money_of_stage_in_build(
+                                                              build_stage.buildkey,
+                                                              build_stage.stagekey
+                                   );
+            FETCH curs_build_stage INTO build_stage;
+        END LOOP;
+
+        RETURN allmoney;
+    EXCEPTION
+        WHEN timeout_on_resource THEN
+            raise_application_error(
+                                   -20002,
+                                   'Превышен интервал ожидания'
+            );
+        WHEN value_error THEN
+            raise_application_error(
+                                   -20004,
+                                   'Ошибка в операции преобразования или математической операции!'
+            );
+        WHEN err THEN
+            dbms_output.put_line('По этому строительству пока что нет заказов на материалы');
+    END all_money_of_build;
+
+END pack_test;
+/
